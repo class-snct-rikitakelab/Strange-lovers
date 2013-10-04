c0b2ab093845b1476f75de15c11e612f32393d87
SuzukiTomoyuki <dxfsrs1004@gmail.com>
2013-10-04 19:35:51 +0900

    fix slope2

---
+++ Strange_Lovers/StrategyFactories/Out/Slope2Factory.h
@@ -0,0 +1,28 @@
+#ifndef _Slope2Factory_
+#define _Slope2Factory_
+
+
+#include "AfterSlopeStraightFactory.h"
+
+
+//StarategyPart
+
+Section slope2;
+RunningMethod slopeDownRunningMethod;
+RunningMethod slopeDown2RunningMethod;
+
+
+RunningMethod slope2_running_method_array[2];
+
+
+//Section firstCurve;
+
+
+//StrategyPart/RunningMethodSwitchJudge
+
+
+extern void slope2_factory_init();
+
+#endif
+
+
